324930d1d22dbb38065f36a9e4e7e37b70986b62
Jonathan Mutakura <jmutakura@gmail.com>
2019-11-03 20:40:36 -0600

    Cleaned up the file structure and updated the controllers and routes

--- app.js
+++ app.js
@@ -1,59 +1,30 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//1)MIDDLEWARE
+app.use(morgan('dev'));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from the server!', app: 'JTours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-
-//get request 
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours
-    }
-  })
+app.use((req, res, next) => {
+  console.log('hello from the middleware⚜️ 🔱');
+  next();
 });
 
-//Post request
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({
-    id: newId
-  }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-
-    });
-  });
-
+app.use((req, res, next) => {
+  req.resquestTime = new Date().toISOString();
+  next();
 });
 
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+//3)ROUTES
+
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
